38631eadbc75de6ed47cb09a59e04b61b142245a
dminet1 <minet.david@voila.fr>
2016-05-18 11:29:25 +0200

    Ajout boutons suppression et mise à jour

--- controleurs/controleur.js
+++ controleurs/controleur.js
@@ -5,5 +5,18 @@ myModule.controller("productsCtrl", function ($scope) {
         new product("Bracelet", "Bijou", "35.00€"),
         new product("Bermuda", "Vêtement", "14.75€")
     ];
-});
+    $scope.delete = function (idx) {
+        var product_to_delete = $scope.products[idx];
 
+        API.DeleteProduct({id: product_to_delete.id}, function (success) {
+            $scope.products.splice(idx, 1);
+        });
+    };
+    $scope.update = function (idx) {
+        var product_to_update = $scope.products[idx];
+
+        API.UpdateProduct({id: product_to_update.id}, function (success) {
+            $scope.products.splice(idx, 1);
+        });
+    };
+});
